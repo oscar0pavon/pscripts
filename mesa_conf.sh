11eb873790c0884dfba5cecfd54f5c57e18c31cc
Oscar Pavon <pavon_oscar@outlook.com>
2026-03-27 22:10:14 -0300

    added a lot of new scripts

--- mesa_conf.sh
+++ mesa_conf.sh
@@ -2,10 +2,11 @@
 
 meson setup ..                 \
       --prefix=$XORG_PREFIX    \
-      --buildtype=release      \
+      --buildtype=debug \
       -D platforms=x11,wayland \
-      -D gallium-drivers=i915,iris  \
-      -D vulkan-drivers=intel,intel_hasvk   \
+      -D gallium-drivers=i915,iris,radeonsi,llvmpipe\
+      -D egl-native-platform=auto\
+      -D vulkan-drivers=intel,intel_hasvk,amd   \
       -D valgrind=disabled     \
       -D video-codecs=all      \
       -D libunwind=disabled --reconfigure
